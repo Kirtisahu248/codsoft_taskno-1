ac80cf86cefbaffbbb1754bf7a6e6ee4f8c7d973
Kirti Sahu <sahukirti1804@gmail.com>
2023-07-30 21:19:23 +0530

    first commit

---
+++ JS/timer.js
@@ -0,0 +1,21 @@
+var countDownDate = new Date();
+countDownDate.setDate(countDownDate.getDate() + 50);
+countDownDate.setHours(0, 0, 0, 0);
+countDownDate = countDownDate.getTime();
+
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
